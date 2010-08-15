5a77a25d07e126456c7250d24d07a0d0a122d7e4
Arley Lobato <arleylobato@gmail.com>
2010-08-15 10:24:59 -0300

    Atualiza evento para WhyDay e corrige bug no javascript

--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
 require 'rubyonrio'
-
 run RubyOnRio
--- public/application.js
+++ public/application.js
@@ -1,8 +1,8 @@
-$(function() {
+$(document).ready(function() {
 	$(".tweet").tweet({
 		avatar_size: 32,
-    count: 6,
-    query: "#rubyonrio",
+	    count: 6,
+	    query: "#rubyonrio",
 		loading_text: "carregando tweets..."
 	});
 
@@ -11,7 +11,7 @@ $(function() {
     return (num == 1 ? ret : ret + 's')
   }
 
-  var launchDate = new Date(1281790800000)
+  var launchDate = new Date(1282186800000)
   var countdown = function(){
     var difference = Math.floor((launchDate - new Date())/1000)
     if (difference < 0) document.location.reload(true)
@@ -19,14 +19,14 @@ $(function() {
     var minutes = Math.floor((difference - seconds)/60) % 60
     var hours = Math.floor((difference - seconds - minutes*60)/60/60) % 24
     var days = Math.floor((difference - seconds - minutes*60 - hours*60*60)/60/60/24)
-
+  
     if (days > 0) $('#days_count').text(plural(days, 'dia') + ',')
     if (hours > 0) $('#hours_count').text(plural(hours, 'hora') + ',')
     if (minutes > 0) $('#minutes_count').text(plural(minutes, 'minuto') + ' e ')
     $('#seconds_count').text(plural(seconds, 'segundo'))
-
+  
     $('#countdown').show()
   }
-  countdown()
+
   setInterval(countdown, 1000)
 });
